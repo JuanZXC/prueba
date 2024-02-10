5e34977684fc4e53238a53634087f49d1ac17bb0
Juan <a340700746@gmail.com>
2024-02-10 17:52:32 +0100

    commit 2

--- Prueba-Git/src/com/curso/git/Gestor.java
+++ Prueba-Git/src/com/curso/git/Gestor.java
@@ -1,9 +1,13 @@
 package com.curso.git;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gestor {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		List<String> list = List.of("1", "2", "3");
 	}
 
 }
